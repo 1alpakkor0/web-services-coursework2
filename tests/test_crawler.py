469a35e0e3a09ba5ebf636b750af29e9d6c265ae
Alp Akkor <146676554+1alpakkor0@users.noreply.github.com>
2026-04-30 10:34:18 +0000

    added couple of tests for crawler

---
+++ tests/test_crawler.py
@@ -0,0 +1,38 @@
+from src.crawler import Crawler
+
+
+SAMPLE_HTML = """
+<html>
+<body>
+    <div class="quote">
+        <span class="text">“The world as we have created it is a process of our thinking.”</span>
+        <small class="author">Albert Einstein</small>
+    </div>
+    <li class="next">
+        <a href="/page/2/">Next</a>
+    </li>
+</body>
+</html>
+"""
+
+
+def test_parse_quotes_extracts_quote_and_author():
+    crawler = Crawler(politeness_delay=0)
+
+    quotes = crawler.parse_quotes(SAMPLE_HTML, "https://quotes.toscrape.com/")
+
+    assert len(quotes) == 1
+    assert quotes[0]["author"] == "Albert Einstein"
+    assert "world" in quotes[0]["text"]
+    assert quotes[0]["url"] == "https://quotes.toscrape.com/"
+
+
+def test_get_next_page_url_returns_absolute_url():
+    crawler = Crawler(politeness_delay=0)
+
+    next_url = crawler.get_next_page_url(
+        SAMPLE_HTML,
+        "https://quotes.toscrape.com/"
+    )
+
+    assert next_url == "https://quotes.toscrape.com/page/2/"
