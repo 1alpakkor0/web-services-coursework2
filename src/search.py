f704c81f3fe6f122150c0aaab09915f82b21d6cd
Alp Akkor <146676554+1alpakkor0@users.noreply.github.com>
2026-04-30 16:29:29 +0000

    implemented TF-IDF ranking into the search

--- src/search.py
+++ src/search.py
@@ -1,3 +1,4 @@
+import math
 import re
 
 
@@ -18,6 +19,24 @@ class SearchEngine:
 
         return self.index[word]
 
+    def calculate_tfidf_score(self, term, doc_id):
+        """
+        Calculate TF-IDF score for a term in a document.
+
+        TF = frequency of the term in the document
+        IDF = log((N + 1) / (df + 1)) + 1
+
+        Smoothing is used to avoid division by zero.
+        """
+        total_documents = len(self.documents)
+        document_frequency = len(self.index.get(term, {}))
+
+        term_frequency = self.index[term][doc_id]["frequency"]
+
+        idf = math.log((total_documents + 1) / (document_frequency + 1)) + 1
+
+        return term_frequency * idf
+
     def find(self, query):
         query_terms = self.tokenize_query(query)
 
@@ -37,14 +56,14 @@ class SearchEngine:
 
         for doc_id in matching_doc_ids:
             score = sum(
-                self.index[term][doc_id]["frequency"]
+                self.calculate_tfidf_score(term, doc_id)
                 for term in query_terms
             )
 
             results.append({
                 "doc_id": doc_id,
                 "url": self.documents[doc_id]["url"],
-                "score": score,
+                "score": round(score, 4),
                 "matched_terms": query_terms
             })
 
